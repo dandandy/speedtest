546827325bc58238b965fb765dc0846a9f03a549
Daniel Barrett <barrett.t.daniel@gmail.com>
2020-04-29 20:11:35 +1000

    Run create table script

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ source ./env.sh
 
 docker run speedtest --progress=no --format=csv > data/data.csv
 
+docker run -i --net=host -v $(pwd)/data:/data mysql mysql --local-infile=1 -h "${host}" -u "${user}" --password="${password}" --database="${database}" < create.sql
 docker run -i --net=host -v $(pwd)/data:/data mysql mysql --local-infile=1 -h "${host}" -u "${user}" --password="${password}" --database="${database}" < insert.sql
 
 docker run -i --net=host -v $(pwd)/data:/data mysql mysql --local-infile=1 -h "${host}" -u "${user}" --password="${password}" --database="${database}" < <(echo "select * from network_logs order by ts DESC limit 3")
